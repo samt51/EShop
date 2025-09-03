d80120d26e41333102727214029adf775eb6f101
mucahitsamet <samet@sadecemotor.com>
2025-09-03 04:04:58 +0300

    catalog servisine repository ve unitofwork interface oluşturdum.

---
+++ EShopSln/Catalog.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -0,0 +1,12 @@
+using EShop.Shared.Dtos.Common;
+
+namespace Catalog.Application.Interfaces.Repositories;
+
+public interface IWriteRepository<T> where T : class, IBaseEntity, new()
+{
+    Task<T> AddAsync(T entity);
+    Task AddRangeAsync(IList<T> entities);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+    Task HardDeleteRangeAsync(IList<T> entity);
+}
